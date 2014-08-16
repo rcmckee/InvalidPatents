ab041bde4e8c6250e2bf6a534ebd931b2b161968
rcmckee <rmckee8@gmail.com>
2014-08-15 18:31:07 -0700

    writing output pubNum to txt file

--- _1stgoogleparsetotxtfile.py
+++ _1stgoogleparsetotxtfile.py
@@ -8,12 +8,13 @@ import re
 
 #***open files with a loop WITH statement that will close them after it is done
 #test folder with 5 google html files is desktop/gHtmlTestFolder
+
 folder = '/Users/robertmckee/Desktop/gHtmlTestFolder'
 ginput_fileList = os.listdir(folder) #creats list of files in folder
 #with open (ginput_file, "r") as myfile:   #http://goo.gl/7osxn6 got this code from this thread
 #    text=myfile.read()
 #raw = nltk.clean_html(text)
-
+lensUrlListFile = open('/Users/robertmckee/Tresors/Projects/InvalidPatents/_LensUrlList.txt',"a")
 for file in ginput_fileList:
   file_name = '/Users/robertmckee/Desktop/gHtmlTestFolder/' + file
   with open(file_name, 'r') as myfile:
@@ -30,6 +31,7 @@ for file in ginput_fileList:
   pubNumLast = pubNumNum[2].partition('Publication')
   pubNum = pubNumUS[1]+'_'+pubNumNum[0]+'_'+pubNumLast[0]
   print int_pubNum[1]+pubNum
+  lensUrlListFile.write(pubNum+'\n')
 
   #**assgnee
   int_assgnee = raw.partition('Original Assignee ')
@@ -40,18 +42,6 @@ for file in ginput_fileList:
   #**canceled or cancelled
   int_legalEventsString = assgneeName[2].partition('Legal Events')
   legalEventsString = int_legalEventsString[2].partition('Legal Events')
- # if len(legalEventsString[2]) < 1:
- #   print 'No Legal Information'
- # elif re.search('canceled', legalEventsString[2], re.IGNORECASE) == Match:
- #   print 'Canceled Claims'
- # elif re.search('cancelled', legalEventsString[2], re.IGNORECASE) == Match:
- #   print 'Canceled Claims'
- # else:
- #   print 'No Canceled Claims'
- # if re.search('Legal Events', legalEventsString[1], re.IGNORECASE) == False:
- #   print 'No Legal Events available'
- # if len(legalEventsString[2])<1:
- #   print 'No Legal Events Available! Check!'
   matchCanceled = re.search(r"(canceled|cancelled|CANCELED|CANCELLED)", legalEventsString[2]) #the '|' separates possibilites it could be http://flockhart.virtualave.net/RBIF0100/regexp.html
   if matchCanceled:
     print 'Canceled Claims!'
@@ -59,7 +49,13 @@ for file in ginput_fileList:
     print 'No Legal Events Available! Check!'
   else:
     print 'No Canceled Claims'
-#***create/write/append to a txt file or database?
+#***create/write/append to a txt file for url list of lens.org scraping
 
 
-#***
+
+#*** create/write to a database
+
+
+#*** close database and listfile when the loop is over. so keep it against the left side. no indents.
+
+lensUrlListFile.close()
