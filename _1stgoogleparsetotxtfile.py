ff29a90c891499dc0d0982aebb61c4f1b672a748
rcmckee <rmckee8@gmail.com>
2014-08-17 12:55:34 -0700

    scrape google and upload to new db
    created code to scrape google html files and upload to new database. It
    also prints out a list of urls to scrape from lens.

--- _1stgoogleparsetotxtfile.py
+++ _1stgoogleparsetotxtfile.py
@@ -4,8 +4,9 @@ import nltk
 import sys
 import os.path
 import re
+import sqlite3
 
-
+db = sqlite3.connect('_googlePgsDb.sqlite') #put this up here so I'm not continually opening and closing the database with every file in the folder I loop through.
 #***open files with a loop WITH statement that will close them after it is done
 #test folder with 5 google html files is desktop/gHtmlTestFolder
 
@@ -14,6 +15,7 @@ ginput_fileList = os.listdir(folder) #creats list of files in folder
 #with open (ginput_file, "r") as myfile:   #http://goo.gl/7osxn6 got this code from this thread
 #    text=myfile.read()
 #raw = nltk.clean_html(text)
+counter = 0
 lensUrlListFile = open('/Users/robertmckee/Tresors/Projects/InvalidPatents/_LensUrlList.txt',"a")
 for file in ginput_fileList:
   file_name = '/Users/robertmckee/Desktop/gHtmlTestFolder/' + file
@@ -32,6 +34,7 @@ for file in ginput_fileList:
   pubNum = pubNumUS[1]+'_'+pubNumNum[0]+'_'+pubNumLast[0]
   print int_pubNum[1]+pubNum
   lensUrlListFile.write(pubNum+'\n') #the '\n' causes it to do a new line.
+  patNum = pubNumNum[0]
 
   #**filing date [filingDat]
   int_filingDat = raw.partition('Filing date ')
@@ -40,6 +43,10 @@ for file in ginput_fileList:
   int_filingDatYr = int_filingDatDay[2].partition('Priority date')
   filingDat = int_filingDat[1]+int_filingDatYr[0]+' '+int_filingDatMnth[0]+' '+int_filingDatDay[0]
   print filingDat
+  filingDatYr = int_filingDatYr[0]
+  filingDatMnth = int_filingDatMnth[0]
+  filingDatDay = int_filingDatDay[0]
+
 
   #**assgnee
   int_assgnee = int_filingDatYr[2].partition('Original Assignee ')
@@ -52,18 +59,35 @@ for file in ginput_fileList:
   legalEventsString = int_legalEventsString[2].partition('Legal Events')
   matchCanceled = re.search(r"(canceled|cancelled|CANCELED|CANCELLED)", legalEventsString[2]) #the '|' separates possibilites it could be http://flockhart.virtualave.net/RBIF0100/regexp.html
   if matchCanceled:
+    cancledClaims = 'Yes'
     print 'Canceled Claims!'
   elif len(legalEventsString[2])<1:
+    cancledClaims = 'Verify'
     print 'No Legal Events Available! Check!'
   else:
+    cancledClaims = 'No'
     print 'No Canceled Claims'
 #***create/write/append to a txt file for url list of lens.org scraping
   #already done further up
 
 
-#*** create/write to a database
-
+#*** write to a database
+  db.text_factory = str
+  cursor = db.cursor()
+  cursor.execute('insert into patent values (?,?,?,?,?,?,?,?)',(pubNum, assgnee, filingDatYr, filingDatMnth, filingDatDay, cancledClaims, file_name, patNum))
+  db.commit()
+  counter += 1
+  print 'Files processed so far: ', counter
+#file_name
+#pubNumNum  #this is the patent number that I used to find them in google
+#pubNum  #This is the critical one I'm looking for
+#int_filingDatYr
+#int_filingDatMnth
+#int_filingDatDay
+#assgnee
+#cancledClaims
 
 #*** close database and listfile when the loop is over. so keep it against the left side. no indents.
-
+print 'Total Number of files processed: ', counter
 lensUrlListFile.close()
+db.close()
